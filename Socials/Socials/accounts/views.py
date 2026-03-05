67ca005def7710fb8e56c4f62d6c3e9ef363eb2d
msaim995566-lab <msaim995566@gmail.com>
2026-03-05 15:01:53 +0500

    hi

--- Socials/Socials/accounts/views.py
+++ Socials/Socials/accounts/views.py
@@ -4,5 +4,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 class HomeView(LoginRequiredMixin,generic.TemplateView):
+    #hi
     template_name = "home.html"
     login_url = 'account_login'
